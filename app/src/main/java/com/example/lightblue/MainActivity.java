24a32e683f7e171cd0645c8305f32701e99049e3
RamloA <36705833+RamloA@users.noreply.github.com>
2018-04-04 18:42:50 +0200

    1

--- app/src/main/java/com/example/lightblue/MainActivity.java
+++ app/src/main/java/com/example/lightblue/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.lightblue;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
 import com.punchthrough.bean.sdk.Bean;
 import com.punchthrough.bean.sdk.BeanDiscoveryListener;
 import com.punchthrough.bean.sdk.BeanManager;
@@ -11,32 +23,116 @@ import com.punchthrough.bean.sdk.BeanManager;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    BeanDiscoveryListener listener;
 
+public class MainActivity extends AppCompatActivity {
+    final List<Bean>  beans = new ArrayList<>();
+    LocationManager locationManager;
+    //final Bean bean = beans[0];
+
+    //Name = "98:7B:F3:5A:CE:D9";
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("")
+                        .setMessage("")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        int REQUEST_ENABLE_BT = 1;
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        checkLocationPermission();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-    }
-    public void Onconnect(View view) {
-        BeanManager.getInstance().setScanTimeout(15);  // Timeout in seconds, optional, default is 30 seconds
+        BeanManager.getInstance().setScanTimeout(10);
         BeanManager.getInstance().startDiscovery(listener);
+    }
+
+    public void Onconnect(View view) {
 
     }
 
-    final List<Bean> beans = new ArrayList<>();
 
     BeanDiscoveryListener listener = new BeanDiscoveryListener() {
         @Override
         public void onBeanDiscovered(Bean bean, int rssi) {
             beans.add(bean);
+            Log.d("Bluetooth", String.valueOf(bean.getDevice().getName()));
+            Log.d("Address",String.valueOf(bean.getDevice().getAddress()));
         }
 
         @Override
@@ -44,12 +140,17 @@ public class MainActivity extends AppCompatActivity {
             // This is called when the scan times out, defined by the .setScanTimeout(int seconds) method
 
             for (Bean bean : beans) {
+                Log.d("Bluetooth", String.valueOf(bean.getDevice().getName()));
                 System.out.println(bean.getDevice().getName());   // "Bean"              (example)
+                Log.d("Address",String.valueOf(bean.getDevice().getAddress()));
                 System.out.println(bean.getDevice().getAddress());    // "B4:99:4C:1E:BC:75" (example)
             }
 
         }
     };
+
+
+
 
 
 }
